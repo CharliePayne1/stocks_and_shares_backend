a63e94add9ba6a6bcac8a130b918a41f8727c84b
CharliePayne1 <charliepayne1101@gmail.com>
2020-07-06 18:50:35 +0100

    searchbar added

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,7 +1,10 @@
 class StocksController < ApplicationController
 
   def index
-    response = HTTParty.get("http://api.marketstack.com/v1/eod?access_key=#{ENV['API_SECRET_KEY']}&symbols=MSFT,AAPL,AMZN,GOOG,GOOGL,FB,VOD,INTC,CMCSA,PEP,ADBE,CSCO,NVBA,NFLX,TSLA,COST,PYPL,AMGN,FNY,ASML", headers: { 
+    builtUrl = "http://api.marketstack.com/v1/eod?access_key=#{ENV['API_SECRET_KEY']}&symbols=MSFT,AAPL,AMZN,GOOG,GOOGL,FB,VOD,INTC,CMCSA,PEP,ADBE,CSCO,NVBA,NFLX,TSLA,COST,PYPL,AMGN,FNY,ASML"
+    builtUrl += "&date_from=#{params[:date]}T00:00:00+0000&date_to=#{params[:date]}T00:00:00+0000" if params[:date]
+    
+    response = HTTParty.get(builtUrl, headers: { 
       "Accept" => "application/json"
     })
     render json: response.to_h
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
 
   resources :sectors
 
-  resources :stocks, only: [:index, :new, :create, :edit, :update, :delete]
+  resources :stocks, only: [:new, :create, :edit, :update, :delete]
+
+  get '/stocks', to: 'stocks#index'
+  post '/stocks', to: 'stocks#index'
 
   get '/stocks/:stock_symbol', to: 'stocks#show'
 
